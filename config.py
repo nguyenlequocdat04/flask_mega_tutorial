e0fa9c341f466dbba73ac2397d5f5f56c28c8475
Nguyễn Lê Quốc Đạt <nguyenlequocdat04@gmail.com>
2019-11-07 10:14:53 +0700

    Update config.py

--- config.py
+++ config.py
@@ -3,3 +3,4 @@ import os
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     MONGO_DB = os.environ.get('MONGO_DB') or 'mongodb://172.18.0.2:27017/fplayux'
+    #ádasdasdasd
